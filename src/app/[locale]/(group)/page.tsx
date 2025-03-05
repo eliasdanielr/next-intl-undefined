c337f10cc223e2afbd9b1c6c8eb513b68fb2f849
eliasdanielr <eliasdanielrm@gmail.com>
2025-03-05 09:55:19 -0400

    add force-static and routes group

--- src/app/[locale]/(group)/page.tsx
+++ src/app/[locale]/(group)/page.tsx
@@ -1,8 +1,12 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
+export const dynamic = "force-static";
+
+
 export default function Home() {
 	const t = useTranslations("home");
+	
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
